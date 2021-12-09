229682471697d2c17ed972ba914b891a165a41c5
Ax <attigmohammed@gmail.com>
2021-12-09 22:08:42 +0100

    clean apis and removed mqtt

---
+++ routes/posts.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post.model");
+
+//get devices
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//register device
+router.post("/register", async (req, res) => {
+  const post = new Post({
+    rfid: req.body.rfid,
+    user_name: req.body.user_name,
+    mac_wifi: req.body.mac_wifi,
+  });
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//specific post by id
+router.get("/:rfid", async (req, res) => {
+  Post.find({ rfid: req.params.rfid }, function (err, rfid) {
+    if (err) res.send(err);
+
+    res.json(rfid);
+  });
+});
+
+module.exports = router;
